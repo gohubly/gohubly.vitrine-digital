ff21e91a5abf5b36857db0d8cdb175ecf21ca426
Lucas Eduardo <lucased78@hotmail.com>
2022-04-27 16:38:47 -0300

    feat: adding Icon component

---
+++ react/components/Icons/index.ts
@@ -0,0 +1,11 @@
+import Facebook from "./Facebook"
+import Instagram from "./Instagram"
+import Tiktok from "./Tiktok"
+import Youtube from "./Youtube"
+
+export default {
+  Facebook,
+  Instagram,
+  Tiktok,
+  Youtube
+}
