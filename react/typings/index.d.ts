c3f7887bf563e198ac1225af425cc5f7186bf3be
Lucas Eduardo <lucased78@hotmail.com>
2022-04-27 16:38:00 -0300

    feat: adding mock

---
+++ react/typings/index.d.ts
@@ -0,0 +1,13 @@
+type SocialMedia = "facebook" | "instagram" | "tiktok" | "youtube"
+
+type AffiliateStore = {
+  avatar: string,
+  banner: string,
+  name: string,
+  description: string,
+  facebook: string,
+  youtube: string,
+  instagram: string,
+  tiktok: string,
+  slug: string,
+}
